d12f7ebdb4dd9e5d97e66e77b8c12bda8fddbda1
Wong Qian Xian <wongqx9516@gmail.com>
2025-04-13 15:44:12 +0800

    Added objects layer for the castle

---
+++ Tiled (data) qianxian/tsx/Objects.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Objects" tilewidth="768" tileheight="768" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../../../Downloads/castle.png" width="768" height="768"/>
+ </tile>
+ <tile id="1">
+  <image source="../../../../Downloads/mushroom_1.png" width="64" height="64"/>
+ </tile>
+ <tile id="2">
+  <image source="../../../../Downloads/tower.png" width="384" height="384"/>
+ </tile>
+ <tile id="3">
+  <image source="../../../../Downloads/tree_1.png" width="256" height="256"/>
+ </tile>
+</tileset>
