c132f4f16aaaa9fd892649a3d9d61d4f71e08860
Wong Qian Xian <wongqx9516@gmail.com>
2025-04-14 21:36:38 +0800

    added the class to generate the image from tiled

--- code/qianxian.py
+++ code/qianxian.py
@@ -1,6 +1,12 @@
 import pygame, sys
 from pytmx.util_pygame import load_pygame
 
+class Tile(pygame.sprite.Sprite):
+    def __init__(self,pos,surf,groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_rect(topleft = pos)
+
 pygame.init()
 screen = pygame.display.set_mode((1280,720))
 tmx_data = load_pygame("Tiled (data) qianxian/tmx/2d world map.tmx")
