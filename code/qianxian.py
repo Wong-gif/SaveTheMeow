7d409ac188a5c2defa625c87006eac5cca5ea5d2
Wong Qian Xian <wongqx9516@gmail.com>
2025-04-14 21:05:03 +0800

    added the startup of the function to load the map

--- code/qianxian.py
+++ code/qianxian.py
@@ -1,7 +1,9 @@
 import pygame, sys
+from pytmx.util_pygame import load_pygame
 
 pygame.init()
 screen = pygame.display.set_mode((1280,720))
+tmx_data = load_pygame("Tiled (data) qianxian/tmx/2d world map.tmx")
 
 while True:
     for event in pygame.event.get():
