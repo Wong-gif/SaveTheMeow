46c60d5ad7f38050a370819efccca685ae82b942
Wong Qian Xian <wongqx9516@gmail.com>
2025-04-16 01:23:16 +0800

    Setting up the width and height of the window

--- code/qianxian.py
+++ code/qianxian.py
@@ -1,29 +1,5 @@
-import pygame, sys
-from pytmx.util_pygame import load_pygame
+import pygame
+from sys import exit
 
-class Tile(pygame.sprite.Sprite):
-    def __init__(self,pos,surf,groups):
-        super().__init__(groups)
-        self.image = surf
-        self.rect = self.image.get_rect(topleft = pos)
-
-pygame.init()
-screen = pygame.display.set_mode((1280,720))
-tmx_data = load_pygame("Tiled (data) qianxian/tmx/2d world map.tmx")
-sprite_group = pygame.sprite.Group()
-
-for layer in tmx_data.visible_layers:
-    if hasattr(layer,"data"):
-        for x,y,surf in layer.tiles():
-            pos =(x*128,y*128)
-            Tile(pos = pos, surf = surf, groups = sprite_group)
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-    screen.fill("black")
-    sprite_group.draw(screen)
-    pygame.display.update()
+window_width, window_height = 1280, 720
+tile_size = 128
