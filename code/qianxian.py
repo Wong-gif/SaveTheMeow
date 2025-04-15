a4f9e11cc76381c211b5bdd7169f553a34145297
Wong Qian Xian <wongqx9516@gmail.com>
2025-04-16 01:25:39 +0800

    created a class that runs the main code for the game.

--- code/qianxian.py
+++ code/qianxian.py
@@ -3,3 +3,11 @@ from sys import exit
 
 window_width, window_height = 1280, 720
 tile_size = 128
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((window_width,window_height))
+        pygame.display.set_caption("Save The Meow")
+
+    
