02249e7d2afb879250518b14544ef9d6eedcc0c9
Wong Qian Xian <wongqx9516@gmail.com>
2025-04-16 11:18:05 +0800

    world map able to be generated

--- code/qianxian.py
+++ code/qianxian.py
@@ -21,14 +21,14 @@ class Game:
         self.all_sprites = pygame.sprite.Group()
 
         self.import_map()
-        self.setup(self.tmx_maps["world"],"")
+        self.setup(self.tmx_maps["world"],"(0,0)")
 
     def import_map(self):
         self.tmx_maps = {"world": load_pygame(join("Tiled (data) qianxian", "tmx", "2d world map.tmx"))}
 
-    def setup(self, tmx_map, player_start_pos):
+    def setup(self, tmx_map,player_start_pos):
         for x,y,surf in tmx_map.get_layer_by_name("Floor").tiles():
-         print(x,y,surf)
+         Sprite((x*tile_size,y*tile_size),surf,self.all_sprites)
 
     def run(self):
         while True:
@@ -37,6 +37,7 @@ class Game:
                     pygame.quit()
                     exit()
 
+            self.all_sprites.draw(self.display_surface)
             pygame.display.update()
 
 if __name__ == "__main__":
