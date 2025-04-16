d0789e375fd47c3ed2ec960d9d2984af2269f83c
Wong Qian Xian <wongqx9516@gmail.com>
2025-04-16 10:37:45 +0800

    import maps using the join method from os.path

--- code/qianxian.py
+++ code/qianxian.py
@@ -1,6 +1,7 @@
 import pygame
 from sys import exit
 from pytmx.util_pygame import load_pygame
+from os.path import join
 
 window_width, window_height = 1280, 720
 tile_size = 128
@@ -11,8 +12,11 @@ class Game:
         self.display_surface = pygame.display.set_mode((window_width,window_height))
         pygame.display.set_caption("Save The Meow")
 
+        self.import_map()
+
     def import_map(self):
-        self.tmx_maps = {"world": load_pygame("Tiled (data) qianxian/tmx/2d world map.tmx")}
+        self.tmx_maps = {"world": load_pygame(join("Tiled (data) qianxian", "tmx", "2d world map.tmx"))}
+        print(self.tmx_maps)
 
     def run(self):
         while True:
