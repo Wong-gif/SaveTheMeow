a9966f116ec5251ae0ece96c65021093a14c3db5
Wong Qian Xian <wongqx9516@gmail.com>
2025-04-21 00:12:46 +0800

    learned to load multiple pictures in a folder for animation purposes

--- code/qx_support.py
+++ code/qx_support.py
@@ -1,6 +1,16 @@
 from qx_settings import *
 from os.path import join
+from os import walk
 
 def import_image(*path, alpha = True, format = "png"):
     full_path = join(*path) + f".{format}"
     return pygame.image.load(full_path).convert if alpha else pygame.load.image(full_path).convert()
+
+def import_folder(*path):
+    frames = []
+    for folder_path, sub_folders, image_names in walk(join(*path)):
+        for image_name in sorted(image_names, key = lambda name : int(name.split(".")[0])):
+            full_path = join(folder_path,image_name)
+            surf = pygame.image.load(full_path).convert_alpha()
+            frames.append(surf)
+    return frames
