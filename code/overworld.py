7dd5da5340f8442a551e6ad9bbd9773af91dc89d
Wong Qian Xian <wongqx9516@gmail.com>
2025-04-20 23:20:27 +0800

    started working in the main python file to run the overworld map from Tiled

--- code/overworld.py
+++
@@ -1,6 +0,0 @@
-from qx_settings import *
-
-class Overworld:
-    def __init__(self, tmx_map, data, overworld_frames):
-        self.display_surface = pygame.display.get_surface()
-        self.data = data 
--- code/qx_main.py
+++ code/qx_main.py
@@ -1,6 +1,7 @@
 from qx_settings import *
 from pytmx.util_pygame import load_pygame
 from os.path import join
+from qx_overworld import Overworld
 
 class Game:
     def __init__(self):
@@ -8,6 +9,10 @@ class Game:
         self.display_surface = pygame.display.set_mode((window_width,window_height))
         pygame.display.set_caption("Save The Meow")
         self.clock = pygame.time.Clock()
+        self.import_assets()
+
+        #generate the world map
+        self.tmx_overworld = load_pygame(join("tiled_data_qianxian","tmx","overworld.tmx"))
 
     def run(self):
         while True:
