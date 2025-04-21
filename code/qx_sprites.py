ec2fdedaf7db4952c7fc5718e936f95a2fe1a96d
Wong Qian Xian <wongqx9516@gmail.com>
2025-04-21 21:03:20 +0800

    added the z layers into the sprite parameter

--- code/qx_sprites.py
+++ code/qx_sprites.py
@@ -1,7 +1,9 @@
 from qx_settings import *
 
 class Sprite(pygame.sprite.Sprite):
-    def __init__(self,pos,surf,groups):
+    def __init__(self,pos,surf = pygame.Surface((tile_size,tile_size)),groups = None, z = Z_layers["main"]):
         super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_frect(topleft = pos)
+        self.old_rect = self.rect.copy()
+        self.z = z
