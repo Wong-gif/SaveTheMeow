9a3a43682d3eb36f0f41cfe58bcd690468ec69ac
Chia Shi Ken <kennkennn0523@gmail.com>
2025-04-22 00:56:19 +0800

    add text on the center of the box

--- code/ken_weapon_market.py
+++ code/ken_weapon_market.py
@@ -4,6 +4,8 @@ import sys
 WIDTH, HEIGHT = 800, 600
 FPS = 60
 WHITE = (255, 255, 255)
+PURPLE =  (255, 215, 0)
+GOLD = (150, 0, 150)
 
 pygame.init()
 
@@ -17,7 +19,7 @@ player_coins = 1500
 player_gems = 500
 
 market_item = {
-    "name": "Golden Sword", "price": 1000, "currency": "coins"
+    "name": "Golden Sword", "price": 1000, "currency": "gems"
 }
 
 running = True
@@ -25,17 +27,22 @@ while running:
     clock.tick(FPS)
     screen.fill(WHITE)
 
-    coins_text = font.render(f"Coins : {player_coins}", True, (0, 255, 0))
-    gems_text = font.render(f"Gems : {player_gems}", True, (150, 0, 150))
+    coins_text = font.render(f"Coins : {player_coins}", True, (GOLD))
+    gems_text = font.render(f"Gems : {player_gems}", True, (PURPLE))
     screen.blit(coins_text, (30, 20))
-    screen.blit(gems_text, (30, 60))
+    screen.blit(gems_text, (30, 50))
 
     item_x = 100
     item_y = 100
     item_box = pygame.Rect(item_x, item_y, 200, 150)
     pygame.draw.rect(screen, (180, 180, 180), item_box)
 
-   
+    name_text = font.render(market_item["name"], True, (WHITE))
+    screen.blit(name_text, (item_x + 100 - name_text.get_width() // 2, item_y + 160))
+
+    color = (PURPLE) if market_item["currency"] == "coins" else (GOLD)
+    price_text = font.render(f"{market_item['price']} {market_item['currency']}", True, color)
+    screen.blit(price_text, (item_x + 100 - name_text.get_width() // 2, item_y + 180))
 
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
