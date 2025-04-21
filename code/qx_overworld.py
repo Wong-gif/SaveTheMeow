faed45aff1c3ef46a1bbdd6ac5b02eb74f12a1ad
Wong Qian Xian <wongqx9516@gmail.com>
2025-04-21 22:13:26 +0800

    created the method to run the water running in the background

--- code/qx_overworld.py
+++ code/qx_overworld.py
@@ -1,5 +1,5 @@
 from qx_settings import *
-from qx_sprites import Sprite
+from qx_sprites import Sprite, AnimatedSprite
 
 class Overworld:
     def __init__(self, tmx_map, overworld_frames):
@@ -14,6 +14,11 @@ class Overworld:
         for layer in ["main","top"]:
          for x, y, surf in tmx_map.get_layer_by_name(layer).tiles():
           Sprite((x*tile_size,y*tile_size), surf, self.all_sprites, Z_layers["bg tiles"])
+
+        #water
+        for col in range(tmx_map.width):
+           for row in range(tmx_map.height):
+              AnimatedSprite((col*tile_size,row*tile_size), overworld_frames["water"],self.all_sprites,Z_layers["bg"])
 
     def run(self,dt):
         self.all_sprites.update(dt)
