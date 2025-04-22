bcc6ba8a3ad48650ad3dd58c1790a3ba63fb5ac8
Chia Shi Ken <kennkennn0523@gmail.com>
2025-04-22 11:13:54 +0800

    add more boxes for weapon

--- code/ken_weapon_market.py
+++ code/ken_weapon_market.py
@@ -19,9 +19,14 @@ font = pygame.font.SysFont("arial", 20)
 player_coins = 1500
 player_gems = 500
 
-market_item = {
-    "name": "Golden Sword", "price": 1000, "currency": "gems"
-}
+market_item = [
+    {"name": "Golden Sword", "price": 1000, "currency": "coins"},
+    {"name": "Golden Sword", "price": 1000, "currency": "gems"},
+    {"name": "Golden Sword", "price": 1000, "currency": "coins"},
+    {"name": "Golden Sword", "price": 1000, "currency": "gems"},
+    {"name": "Golden Sword", "price": 1000, "currency": "coins"},
+    {"name": "Golden Sword", "price": 1000, "currency": "gems"},
+]
 
 running = True
 while running:
@@ -33,17 +38,19 @@ while running:
     screen.blit(coins_text, (30, 20))
     screen.blit(gems_text, (30, 50))
 
-    item_x = 100
-    item_y = 100
-    item_box = pygame.Rect(item_x, item_y, 200, 150)
-    pygame.draw.rect(screen, (180, 180, 180), item_box)
-
-    name_text = font.render(market_item["name"], True, (BLACK))
-    screen.blit(name_text, (item_x + item_box.width // 2 - name_text.get_width() // 2, item_y + 155))
-
-    color = (PURPLE) if market_item["currency"] == "coins" else (GOLD)
-    price_text = font.render(f"{market_item['price']} {market_item['currency']}", True, color)
-    screen.blit(price_text, (item_x + item_box.width // 2 - price_text.get_width() // 2, item_y + 180))
+    for i, item in enumerate(market_item):
+        col = 3
+        x = 100 + (i % col) * 250
+        y = 100 + (i // col) * 250
+        box = pygame.Rect(x, y, 200, 150)
+        pygame.draw.rect(screen, (180, 180, 180), box)
+        
+        name_text = font.render(item["name"], True, (BLACK))
+        screen.blit(name_text, (x + box.width // 2 - name_text.get_width() // 2, y + 155))
+        
+        color = (PURPLE) if item["currency"] == "coins" else (GOLD)
+        price_text = font.render(f"{item['price']} {item['currency']}", True, color)
+        screen.blit(price_text, (x + box.width // 2 - price_text.get_width() // 2, y + 180))
 
 
 
