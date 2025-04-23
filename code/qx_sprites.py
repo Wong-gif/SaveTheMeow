cecddda11e9186777b29589300deb995b1edf68f
Wong Qian Xian <wongqx9516@gmail.com>
2025-04-24 01:17:35 +0800

    added code where in each node it knows which direction it can go

--- code/qx_sprites.py
+++ code/qx_sprites.py
@@ -22,13 +22,14 @@ class AnimatedSprite(Sprite):
         self.animate(dt)
 
 class Node(pygame.sprite.Sprite):
-    def __init__(self,pos,surf,groups,level,data):
+    def __init__(self,pos,surf,groups,level,data,paths):
         super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_frect(center = (pos[0] + tile_size/2, pos[1] + tile_size/2))
         self.z = Z_layers["path"]
         self.level = level
         self.data = data
+        self.paths = paths
 
 class Icon(pygame.sprite.Sprite):
     def __init__(self,pos,groups,frames):
