6a046664b1d81fb884b184c4e6586ad51836d7ff
Wong Qian Xian <wongqx9516@gmail.com>
2025-04-24 00:55:08 +0800

    added the line where the player appears behind the palm

--- code/qx_groups.py
+++ code/qx_groups.py
@@ -10,10 +10,16 @@ class WorldSprites(pygame.sprite.Group):
     def draw(self,target_pos):
         self.offset.x = -(target_pos[0] - window_width / 2)
         self.offset.y = -(target_pos[1] - window_height / 2)
-
+        #background
         for sprite in sorted(self,key = lambda sprite: sprite.z):
-            if sprite.z == Z_layers["path"]:
+            if sprite.z < Z_layers["main"]:
+              if sprite.z == Z_layers["path"]:
                 if sprite.level <= self.data.unlocked_level:
-                    self.display_surface.blit(sprite.image, sprite.rect.topleft + self.offset)
-            else:
-                    self.display_surface.blit(sprite.image, sprite.rect.topleft + self.offset)
+                 self.display_surface.blit(sprite.image, sprite.rect.topleft + self.offset)
+              else:
+                 self.display_surface.blit(sprite.image, sprite.rect.topleft + self.offset)
+
+        #main
+        for sprite in sorted(self, key = lambda sprite: sprite.rect.centery):
+            if sprite.z == Z_layers["main"]:
+                self.display_surface.blit(sprite.image, sprite.rect.topleft + self.offset)
