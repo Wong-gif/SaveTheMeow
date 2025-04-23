9e0906434ac070bb51ceaf6a23ddfa0c32ac1b3d
Wong Qian Xian <wongqx9516@gmail.com>
2025-04-23 13:31:54 +0800

    a

--- code/qx_settings.py
+++ code/qx_settings.py
@@ -1,7 +1,7 @@
 import pygame, sys
 from pygame.math import Vector2 as vector
 
-window_width, window_height = 1200,800
+window_width, window_height = 1000,600
 tile_size = 64
 
 animation_speed = 6
