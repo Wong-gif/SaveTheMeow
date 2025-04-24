445ce8a95bc57952b81dda4e969563a881a1e9ef
Wong Qian Xian <wongqx9516@gmail.com>
2025-04-25 00:01:47 +0800

    updated for moving upwards, and also horizontal left right

--- code/qx_sprites.py
+++ code/qx_sprites.py
@@ -67,8 +67,15 @@ class Icon(pygame.sprite.Sprite):
             self.direction = vector()
 
     def point_collision(self):
-        if self.direction.y == 1 and self.rect.centery >= self.path [0][1]:
-            self.rect.center = self.path[0][1]
+        if self.direction.y == 1 and self.rect.centery >= self.path [0][1] or \
+           self.direction.y == -1 and self.rect.centery <= self.path [0][1]:
+            self.rect.centery = self.path[0][1]
+            del self.path[0]
+            self.find_path()
+        
+        if self.direction.x == 1 and self.rect.centerx >= self.path[0][0] or \
+           self.direction.x == -1 and self.rect.centerx <= self.path[0][0]:
+            self.rect.centerx = self.path[0][0]
             del self.path[0]
             self.find_path()
 
