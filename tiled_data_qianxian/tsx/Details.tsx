ef3e3cf46039f9b10fef74a637cd6b6fa3420042
Wong Qian Xian <wongqx9516@gmail.com>
2025-04-29 07:39:11 +0800

    clone the repo as my initial repo was corrupted

---
+++ tiled_data_qianxian/tsx/Details.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="details" tilewidth="1024" tileheight="320" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../graphics_qx/details.png" width="1024" height="320"/>
+ </tile>
+</tileset>
