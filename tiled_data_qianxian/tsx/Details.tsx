3f5ed28fd31037eee34b0a608aad7186d9c4e170
Wong Qian Xian <wongqx9516@gmail.com>
2025-04-29 00:15:02 +0800

    design the details and map side layer

---
+++ tiled_data_qianxian/tsx/Details.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Details" tilewidth="64" tileheight="64" tilecount="80" columns="16">
+ <image source="../../../../Downloads/Zelda-main/Zelda-main/01 - level/1 - level/graphics/tilemap/details.png" width="1024" height="320"/>
+</tileset>
---
+++ tiled_data_qianxian/tsx/Trees.tsx
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Trees" tilewidth="128" tileheight="128" tilecount="21" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../../../Downloads/Zelda-main/Zelda-main/01 - level/1 - level/graphics/objects/02.png" width="128" height="128"/>
+ </tile>
+ <tile id="1">
+  <image source="../../../../Downloads/Zelda-main/Zelda-main/01 - level/1 - level/graphics/objects/03.png" width="128" height="128"/>
+ </tile>
+ <tile id="2">
+  <image source="../../../../Downloads/Zelda-main/Zelda-main/01 - level/1 - level/graphics/objects/04.png" width="128" height="128"/>
+ </tile>
+ <tile id="3">
+  <image source="../../../../Downloads/Zelda-main/Zelda-main/01 - level/1 - level/graphics/objects/05.png" width="128" height="128"/>
+ </tile>
+ <tile id="4">
+  <image source="../../../../Downloads/Zelda-main/Zelda-main/01 - level/1 - level/graphics/objects/06.png" width="128" height="128"/>
+ </tile>
+ <tile id="5">
+  <image source="../../../../Downloads/Zelda-main/Zelda-main/01 - level/1 - level/graphics/objects/07.png" width="128" height="128"/>
+ </tile>
+ <tile id="6">
+  <image source="../../../../Downloads/Zelda-main/Zelda-main/01 - level/1 - level/graphics/objects/08.png" width="128" height="128"/>
+ </tile>
+ <tile id="7">
+  <image source="../../../../Downloads/Zelda-main/Zelda-main/01 - level/1 - level/graphics/objects/09.png" width="128" height="128"/>
+ </tile>
+ <tile id="8">
+  <image source="../../../../Downloads/Zelda-main/Zelda-main/01 - level/1 - level/graphics/objects/10.png" width="128" height="128"/>
+ </tile>
+ <tile id="9">
+  <image source="../../../../Downloads/Zelda-main/Zelda-main/01 - level/1 - level/graphics/objects/11.png" width="64" height="128"/>
+ </tile>
+ <tile id="10">
+  <image source="../../../../Downloads/Zelda-main/Zelda-main/01 - level/1 - level/graphics/objects/12.png" width="64" height="128"/>
+ </tile>
+ <tile id="11">
+  <image source="../../../../Downloads/Zelda-main/Zelda-main/01 - level/1 - level/graphics/objects/13.png" width="64" height="128"/>
+ </tile>
+ <tile id="12">
+  <image source="../../../../Downloads/Zelda-main/Zelda-main/01 - level/1 - level/graphics/objects/14.png" width="64" height="128"/>
+ </tile>
+ <tile id="13">
+  <image source="../../../../Downloads/Zelda-main/Zelda-main/01 - level/1 - level/graphics/objects/15.png" width="128" height="128"/>
+ </tile>
+ <tile id="14">
+  <image source="../../../../Downloads/Zelda-main/Zelda-main/01 - level/1 - level/graphics/objects/16.png" width="128" height="128"/>
+ </tile>
+ <tile id="15">
+  <image source="../../../../Downloads/Zelda-main/Zelda-main/01 - level/1 - level/graphics/objects/17.png" width="128" height="128"/>
+ </tile>
+ <tile id="16">
+  <image source="../../../../Downloads/Zelda-main/Zelda-main/01 - level/1 - level/graphics/objects/18.png" width="128" height="128"/>
+ </tile>
+ <tile id="17">
+  <image source="../../../../Downloads/Zelda-main/Zelda-main/01 - level/1 - level/graphics/objects/19.png" width="128" height="128"/>
+ </tile>
+ <tile id="18">
+  <image source="../../../../Downloads/Zelda-main/Zelda-main/01 - level/1 - level/graphics/objects/20.png" width="128" height="128"/>
+ </tile>
+ <tile id="19">
+  <image source="../../../../Downloads/Zelda-main/Zelda-main/01 - level/1 - level/graphics/objects/0.png" width="128" height="128"/>
+ </tile>
+ <tile id="20">
+  <image source="../../../../Downloads/Zelda-main/Zelda-main/01 - level/1 - level/graphics/objects/01.png" width="128" height="128"/>
+ </tile>
+</tileset>
