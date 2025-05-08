097aa88f5a1063234b4eba5785df3eda70e3a252
Chia Shi Ken <kennkennn0523@gmail.com>
2025-05-08 23:27:39 +0800

    restore health function have done

--- code/ken_effect.py
+++ code/ken_effect.py
@@ -4,13 +4,30 @@ import sys
 import time
 
 class WeaponEffects:
-    active_effects = {} # Tracks how many left
+    num_of_usage = {}
 
     @staticmethod
     def apply(name, mario, boss):
+
+        max_usage = {
+            "Thunder Axe": 1,
+            "Essence of Renewal": 3
+        
+        }
+
+        if name in max_usage:
+            WeaponEffects.num_of_usage.setdefault(name, 0)
+            if WeaponEffects.num_of_usage[name] >= max_usage[name]:
+                print(f"{name} has reached its usage limit. You can't use it anymore!")
+                return
+            WeaponEffects.num_of_usage[name] += 1
+
         effects = {
             "Thunder Axe": WeaponEffects.thunder_axe,
+            "Essence of Renewal": WeaponEffects.essence_of_renewal,
         }
+
+        
         if name in effects:
             effects[name](mario, boss)
         else:
@@ -22,3 +39,11 @@ class WeaponEffects:
             mario.attack_power = 2000 # Boost Mario's attack power
             mario.power_timer = pygame.time.get_ticks() + 5000  # 15 seconds
             print("Thunder Axe activated! Mario's attack power boosted to 180 for 15 seconds.")
+
+    @staticmethod
+    def essence_of_renewal(mario, boss):
+        restore_health = 50
+        mario.health += restore_health
+        if mario.health > 100:
+            mario.health = 100  # 不超过最大生命
+        print(f"Essence of Renewal activated! Mario restored {restore_health} HP.")
