89caa076be51c4439980df68d715493b5ad6c959
Wong Qian Xian <wongqx9516@gmail.com>
2025-05-11 00:28:03 +0800

    created a level class used to create different groups to split sprites that are drawn as obstacles and those that are for visuals only

--- code/qx_farm_main.py
+++ code/qx_farm_main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from qx_farm_settings import *
+from qx_farm_level import Level
 
 class Game:
     def __init__(self):
@@ -7,16 +8,20 @@ class Game:
         #general setup
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
+        pygame.display.set_caption("Farming Map")
         self.clock = pygame.time.Clock()
+
+        self.level = Level()
 
     def run(self):
         while True:
             for event in pygame.event.get():
-                if event.type == pygame.QUIT():
+                if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
 
             self.screen.fill("black")
+            self.level.run()
             pygame.display.update()
             self.clock.tick(FPS)
 
