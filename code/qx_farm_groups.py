414b05d1c3ab045d2632c8281468f4e5a66eb1cf
Wong Qian Xian <wongqx9516@gmail.com>
2025-05-11 23:51:21 +0800

    updated the tile class to accept different images through surf and also splits the sprites into different groups

--- code/qx_farm_groups.py
+++ code/qx_farm_groups.py
@@ -2,9 +2,10 @@ import pygame
 from qx_farm_settings import *
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self,pos,groups):
+    def __init__(self,pos,groups,sprite_type,surf = pygame.Surface((TILESIZE,TILESIZE))):
         super().__init__(groups)
-        self.image = pygame.image.load("graphics_qx/test/rock.png").convert_alpha()
+        self.sprite_type = sprite_type
+        self.image = surf
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0,-10)
 
