1b1925e4e81f6ad6a0c21172163fda37fc3e466d
Wong Qian Xian <wongqx9516@gmail.com>
2025-05-12 23:41:50 +0800

    linked the overworld to the farming map but havent done the returning back part

---
+++ code/qx_test_farm.py
@@ -0,0 +1,30 @@
+def farming_map():
+    import pygame, sys
+    from qx_farm_settings import WIDTH,HEIGTH,FPS,TILESIZE
+    from qx_farm_level import Level
+
+    class Game:
+        def __init__(self):
+
+            #general setup
+            pygame.init()
+            self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
+            pygame.display.set_caption("Farming Map")
+            self.clock = pygame.time.Clock()
+
+            self.level = Level()
+
+        def run(self):
+            while True:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        sys.exit()
+
+                self.screen.fill("black")
+                self.level.run()
+                pygame.display.update()
+                self.clock.tick(FPS)
+
+    game = Game()
+    game.run()
