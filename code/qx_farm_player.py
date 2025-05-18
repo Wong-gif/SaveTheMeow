6fdf7b91229e7839375a27c1140db399da35b683
Wong Qian Xian <wongqx9516@gmail.com>
2025-05-19 01:37:03 +0800

    added in the bottom right to display the amount of coins based on the coin stats

--- code/qx_farm_player.py
+++ code/qx_farm_player.py
@@ -35,7 +35,7 @@ class Player(pygame.sprite.Sprite):
         self.stats = {"health":100, "energy":60, "attack":10, "magic":4, "speed":5}
         self.health = self.stats["health"]
         self.energy = self.stats["energy"]
-        self.exp = 123
+        self.coins = 6969
         self.speed = self.stats["speed"]
 
     def import_player_assets(self):
