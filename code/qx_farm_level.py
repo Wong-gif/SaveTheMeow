b37824f1f3f0ee2cc695766df7bc8473498025d1
Wong Qian Xian <wongqx9516@gmail.com>
2025-05-19 00:48:57 +0800

    setting up the ui for health and weapons

--- code/qx_farm_level.py
+++ code/qx_farm_level.py
@@ -5,6 +5,7 @@ from qx_farm_tile import Tile
 from qx_support import *
 from random import choice
 from qx_farm_weapons import Weapon
+from qx_farm_ui import UI
 
 class Level:
     def __init__(self):
@@ -21,6 +22,9 @@ class Level:
 
         #sprite
         self.create_map()
+
+        #ui
+        self.ui = UI()
 
     def create_map(self):
         layout = {
@@ -64,6 +68,7 @@ class Level:
         #update and draw game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
+        self.ui.display(self.player)
 
 class YSortCameraGroup(pygame.sprite.Group): #craetes a camera that follows the player and has overlapping effect
     def __init__(self):
---
+++ code/qx_farm_ui.py
@@ -0,0 +1,6 @@
+import pygame
+from qx_farm_settings import *
+
+class UI:
+    def __init__(self):
+        
