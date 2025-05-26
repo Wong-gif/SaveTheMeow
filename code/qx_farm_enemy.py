077096e3db15ea340e03136662b6388f32cc87f9
Wong Qian Xian <wongqx9516@gmail.com>
2025-05-26 18:21:21 +0800

    added in the code to animate the monsters to move towards the player

--- code/qx_farm_enemy.py
+++ code/qx_farm_enemy.py
@@ -60,8 +60,17 @@ class Enemy(Entity):
         else:
             self.status = "idle"
 
+    def actions(self,player):
+        if self.status == "attack":
+            print("attack")
+        elif self.status == "move":
+            self.direction = self.get_player_distance_direction(player)[1]
+        else:
+            self.direction = pygame.math.Vector2()
+
     def update(self):
         self.move(self.speed)
 
     def enemy_update(self,player):
         self.get_status(player)
+        self.actions(player)
