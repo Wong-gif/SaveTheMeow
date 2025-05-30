fa7b1133fd65c1a0ddac54acc79a804ab09a1f2a
Wong Qian Xian <wongqx9516@gmail.com>
2025-05-30 23:29:53 +0800

    added a class for character animation

--- code/qx_farm_particles.py
+++ code/qx_farm_particles.py
@@ -1,6 +1,45 @@
 import pygame
 from qx_support import import_folder_farm
 
+class AnimationPlayer:
+    def __init__(self):
+        self.frames = {
+            #magic
+            "flame" : import_folder_farm("graphics_qx/special_effects_farming/flame/frames"),
+            "aura" : import_folder_farm("graphics_qx/special_effects_farming/aura"),
+            "heal" : import_folder_farm("graphics_qx/special_effects_farming/heal/frames"),
+
+            #attacks
+            "claw" : import_folder_farm("graphics_qx/special_effects_farming/claw"),
+            "slash" : import_folder_farm("graphics_qx/special_effects_farming/slash"),
+            "spark" : import_folder_farm("graphics_qx/special_effects_farming/spark"),
+            "leaf_attack" : import_folder_farm("graphics_qx/special_effects_farming/leaf_attack"),
+            "thunder" : import_folder_farm("graphics_qx/special_effects_farming/thunder"),
+
+            #monster deaths
+            "squid" : import_folder_farm("graphics_qx/special_effects_farming/smoke_orange"),
+            "raccoon" : import_folder_farm("graphics_qx/special_effects_farming/raccoon"),
+            "spirit" : import_folder_farm("graphics_qx/special_effects_farming/nova"),
+            "bamboo" : import_folder_farm("graphics_qx/special_effects_farming/bamboo"),
+
+            #leafs
+            "leaf" : (
+                import_folder_farm("graphics_qx/special_effects_farming/leaf1"),
+                import_folder_farm("graphics_qx/special_effects_farming/leaf2"),
+                import_folder_farm("graphics_qx/special_effects_farming/leaf3"),
+                import_folder_farm("graphics_qx/special_effects_farming/leaf4"),
+                import_folder_farm("graphics_qx/special_effects_farming/leaf5"),
+                import_folder_farm("graphics_qx/special_effects_farming/leaf6"),
+                self.reflect_images(import_folder_farm("graphics_qx/special_effects_farming/leaf1")),
+                self.reflect_images(import_folder_farm("graphics_qx/special_effects_farming/leaf2")),
+                self.reflect_images(import_folder_farm("graphics_qx/special_effects_farming/leaf3")),
+                self.reflect_images(import_folder_farm("graphics_qx/special_effects_farming/leaf4")),
+                self.reflect_images(import_folder_farm("graphics_qx/special_effects_farming/leaf5")),
+                self.reflect_images(import_folder_farm("graphics_qx/special_effects_farming/leaf6"))
+            )
+        }
+
+
 class ParticlesEffect(pygame.sprite.Sprite):
     def __init__(self,pos,animation_frames,groups):
         super().__init__(groups)
