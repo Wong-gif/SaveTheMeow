faabdf0c8c4d296287a21e7e85add92a767c10ed
Wong Qian Xian <wongqx9516@gmail.com>
2025-05-31 00:16:14 +0800

    implemented a death effect when the enemies has died

--- code/qx_farm_enemy.py
+++ code/qx_farm_enemy.py
@@ -4,7 +4,7 @@ from qx_farm_entity import Entity
 from qx_support import *
 
 class Enemy(Entity):
-    def __init__(self,monster_name,pos,groups,obstacle_sprites,damage_player):
+    def __init__(self,monster_name,pos,groups,obstacle_sprites,damage_player,trigger_death_particles):
         #general setup
         super().__init__(groups)
         self.sprite_type = "enemy"
@@ -36,6 +36,7 @@ class Enemy(Entity):
         self.attack_time = None
         self.attack_cooldown = 400
         self.damage_player = damage_player
+        self.trigger_death_particles = trigger_death_particles
 
         #invinbility timer 
         self.vulnerable = True
@@ -125,6 +126,7 @@ class Enemy(Entity):
     def check_death(self):
         if self.health <= 0:
             self.kill()
+            self.trigger_death_particles(self.rect.center,self.monster_name)
 
     def hit_reaction(self):
         if not self.vulnerable:
